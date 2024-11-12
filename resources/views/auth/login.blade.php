9d793fb1924d13808795a183e97223c66f15bb71
Muhamad Amirul <19381137+miesaf@users.noreply.github.com>
2024-11-12 09:37:10 +0800

    Hide password reset

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -40,7 +40,7 @@
                 <br/>
 
                 @if (Route::has('password.request'))
-                <p class="mb-1"> <a href="{{ route('password.request') }}">I forgot my password</a> </p>
+                {{-- <p class="mb-1"> <a href="{{ route('password.request') }}">I forgot my password</a> </p> --}}
                 @endif
 
                 <p class="mb-0"> <a href="{{ route("register") }}" class="text-center">
