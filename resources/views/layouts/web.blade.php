89916c3ff9ef9034d3782aea2f498707814510c1
Muhamad Amirul (MB1M) <19381137+miesaf@users.noreply.github.com>
2025-01-06 21:46:49 +0800

    Downgrade AdminLTE library from version 4 to version 3

--- resources/views/layouts/web.blade.php
+++ resources/views/layouts/web.blade.php
@@ -1,88 +1,134 @@
+
 <!DOCTYPE html>
-<html lang="en"> <!--begin::Head-->
+<html lang="en">
 
 <head>
-    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-    <title>{{ env("APP_NAME") }}</title><!--begin::Primary Meta Tags-->
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="title" content="{{ env("APP_NAME") }}">
-    <meta name="author" content="MieSaF">
-    <meta name="description" content="AdminLTE is a Free Bootstrap 5 Admin Dashboard, 30 example pages using Vanilla JS.">
-    <meta name="keywords" content="bootstrap 5, bootstrap, bootstrap 5 admin dashboard, bootstrap 5 dashboard, bootstrap 5 charts, bootstrap 5 calendar, bootstrap 5 datepicker, bootstrap 5 tables, bootstrap 5 datatable, vanilla js datatable, colorlibhq, colorlibhq dashboard, colorlibhq admin dashboard"><!--end::Primary Meta Tags--><!--begin::Fonts-->
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@fontsource/source-sans-3@5.0.12/index.css" integrity="sha256-tXJfXfp6Ewt1ilPzLDtQnJV4hclT9XuaZUKyUvmyr+Q=" crossorigin="anonymous"><!--end::Fonts--><!--begin::Third Party Plugin(OverlayScrollbars)-->
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/overlayscrollbars@2.3.0/styles/overlayscrollbars.min.css" integrity="sha256-dSokZseQNT08wYEWiz5iLI8QPlKxG+TswNRD8k35cpg=" crossorigin="anonymous"><!--end::Third Party Plugin(OverlayScrollbars)--><!--begin::Third Party Plugin(Bootstrap Icons)-->
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.0/font/bootstrap-icons.min.css" integrity="sha256-Qsx5lrStHZyR9REqhUF8iQt73X06c8LGIUPzpOhwRrI=" crossorigin="anonymous"><!--end::Third Party Plugin(Bootstrap Icons)--><!--begin::Required Plugin(AdminLTE)-->
-    <link rel="stylesheet" href="{{ asset("css/adminlte.css") }}"><!--end::Required Plugin(AdminLTE)--><!-- apexcharts -->
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/apexcharts@3.37.1/dist/apexcharts.css" integrity="sha256-4MX+61mt9NVvvuPjUWdUdyfZfxSB1/Rf9WtqRHgG5S0=" crossorigin="anonymous"><!-- jsvectormap -->
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/jsvectormap@1.5.3/dist/css/jsvectormap.min.css" integrity="sha256-+uGLJmmTKOqBr+2E6KDYs/NRsHxSkONXFHUL0fy2O/4=" crossorigin="anonymous">
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>{{ env("APP_NAME") }}</title>
+
+  <!-- Google Font: Source Sans Pro -->
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+  <!-- Font Awesome -->
+  <link rel="stylesheet" href="{{ asset("plugins/fontawesome-free/css/all.min.css") }}">
+  <!-- Ionicons -->
+  <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
+  <!-- Tempusdominus Bootstrap 4 -->
+  <link rel="stylesheet" href="{{ asset("plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css") }}">
+  <!-- iCheck -->
+  <link rel="stylesheet" href="{{ asset("plugins/icheck-bootstrap/icheck-bootstrap.min.css") }}">
+  <!-- JQVMap -->
+  <link rel="stylesheet" href="{{ asset("plugins/jqvmap/jqvmap.min.css") }}">
+  <!-- Theme style -->
+  <link rel="stylesheet" href="{{ asset("dist/css/adminlte.min.css?v=3.2.0") }}">
+  <!-- overlayScrollbars -->
+  <link rel="stylesheet" href="{{ asset("plugins/overlayScrollbars/css/OverlayScrollbars.min.css") }}">
+  <!-- Daterange picker -->
+  <link rel="stylesheet" href="{{ asset("plugins/daterangepicker/daterangepicker.css") }}">
+  <!-- summernote -->
+  <link rel="stylesheet" href="{{ asset("plugins/summernote/summernote-bs4.min.css") }}">
+
+    <script data-cfasync="false" nonce="0a9167ba-c847-48a2-ac2b-d3a264e31442">try{(function(w,d){!function(a,b,c,d){if(a.zaraz)console.error("zaraz is loaded twice");else{a[c]=a[c]||{};a[c].executed=[];a.zaraz={deferred:[],listeners:[]};a.zaraz._v="5848";a.zaraz._n="0a9167ba-c847-48a2-ac2b-d3a264e31442";a.zaraz.q=[];a.zaraz._f=function(e){return async function(){var f=Array.prototype.slice.call(arguments);a.zaraz.q.push({m:e,a:f})}};for(const g of["track","set","debug"])a.zaraz[g]=a.zaraz._f(g);a.zaraz.init=()=>{var h=b.getElementsByTagName(d)[0],i=b.createElement(d),j=b.getElementsByTagName("title")[0];j&&(a[c].t=b.getElementsByTagName("title")[0].text);a[c].x=Math.random();a[c].w=a.screen.width;a[c].h=a.screen.height;a[c].j=a.innerHeight;a[c].e=a.innerWidth;a[c].l=a.location.href;a[c].r=b.referrer;a[c].k=a.screen.colorDepth;a[c].n=b.characterSet;a[c].o=(new Date).getTimezoneOffset();if(a.dataLayer)for(const k of Object.entries(Object.entries(dataLayer).reduce(((l,m)=>({...l[1],...m[1]})),{})))zaraz.set(k[0],k[1],{scope:"page"});a[c].q=[];for(;a.zaraz.q.length;){const n=a.zaraz.q.shift();a[c].q.push(n)}i.defer=!0;for(const o of[localStorage,sessionStorage])Object.keys(o||{}).filter((q=>q.startsWith("_zaraz_"))).forEach((p=>{try{a[c]["z_"+p.slice(7)]=JSON.parse(o.getItem(p))}catch{a[c]["z_"+p.slice(7)]=o.getItem(p)}}));i.referrerPolicy="origin";i.src="/cdn-cgi/zaraz/s.js?z="+btoa(encodeURIComponent(JSON.stringify(a[c])));h.parentNode.insertBefore(i,h)};["complete","interactive"].includes(b.readyState)?zaraz.init():a.addEventListener("DOMContentLoaded",zaraz.init)}}(w,d,"zarazData","script");window.zaraz._p=async bs=>new Promise((bt=>{if(bs){bs.e&&bs.e.forEach((bu=>{try{const bv=d.querySelector("script[nonce]"),bw=bv?.nonce||bv?.getAttribute("nonce"),bx=d.createElement("script");bw&&(bx.nonce=bw);bx.innerHTML=bu;bx.onload=()=>{d.head.removeChild(bx)};d.head.appendChild(bx)}catch(by){console.error(`Error executing script: ${bu}\n`,by)}}));Promise.allSettled((bs.f||[]).map((bz=>fetch(bz[0],bz[1]))))}bt()}));zaraz._p({"e":["(function(w,d){})(window,document)"]});})(window,document)}catch(e){throw fetch("/cdn-cgi/zaraz/t"),e;};</script>
 
     @yield('head')
-</head> <!--end::Head--> <!--begin::Body-->
+</head>
+<body class="hold-transition sidebar-mini layout-fixed">
+    <div class="wrapper">
 
-<body class="layout-fixed sidebar-expand-lg bg-body-tertiary"> <!--begin::App Wrapper-->
-    <div class="app-wrapper"> <!--begin::Header-->
+    <!-- Navbar -->
+    <nav class="navbar navbar-expand navbar-light">
+        <!-- Left navbar links -->
+        <ul class="navbar-nav">
+            <li class="nav-item">
+            <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
+            </li>
+            <li class="nav-item d-none d-sm-inline-block">
+            <a href="{{ route("welcome") }}" class="nav-link">Home</a>
+            </li>
+            <li class="nav-item d-none d-sm-inline-block">
+            <a href="#" class="nav-link">Contact</a>
+            </li>
+        </ul>
+    </nav>
 
-        <nav class="app-header navbar navbar-expand-lg bg-body-tertiary">
-            <div class="container-fluid">
-                <a class="navbar-brand" href="{{ route("welcome") }}">
-                    <img src="{{ asset('assets/img/georecycle-logo.jpg') }}" height="40px">
-                </a>
+    <!--
+    <nav class="navbar navbar-expand bg-body-success">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="{{ route("welcome") }}">
+                <img src="{{ asset('assets/img/georecycle-logo.jpg') }}" height="40px">
+            </a>
 
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
 
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                        <li class="nav-item"> <a class="nav-link {{ request()->routeIs('welcome') ? 'active' : ''}}" aria-current="page" href="{{ route("welcome") }}">Home</a> </li>
-                        <li class="nav-item"> <a class="nav-link {{ request()->routeIs('map') ? 'active' : ''}}" href="{{ route("map") }}">GeoRecycle Map</a> </li>
-                        <li class="nav-item"> <a class="nav-link {{ request()->routeIs('services') ? 'active' : ''}}" href="{{ route("services") }}">Our Services</a> </li>
-                        <li class="nav-item"> <a class="nav-link {{ request()->routeIs('info') ? 'active' : ''}}" href="{{ route("info") }}">Recycling Information</a> </li>
-                        <li class="nav-item"> <a class="nav-link" href="{{ route("login") }}">Login</a> </li>
-                    </ul>
-                </div>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item"> <a class="nav-link {{ request()->routeIs('welcome') ? 'active' : ''}}" aria-current="page" href="{{ route("welcome") }}">Home</a> </li>
+                    <li class="nav-item"> <a class="nav-link {{ request()->routeIs('map') ? 'active' : ''}}" href="{{ route("map") }}">GeoRecycle Map</a> </li>
+                    <li class="nav-item"> <a class="nav-link {{ request()->routeIs('services') ? 'active' : ''}}" href="{{ route("services") }}">Our Services</a> </li>
+                    <li class="nav-item"> <a class="nav-link {{ request()->routeIs('info') ? 'active' : ''}}" href="{{ route("info") }}">Recycling Information</a> </li>
+                    <li class="nav-item"> <a class="nav-link" href="{{ route("login") }}">Login</a> </li>
+                </ul>
             </div>
-        </nav>
+        </div>
+    </nav> -->
+    <!-- /.navbar -->
 
-        @yield('body')
+    @yield('body')
 
-        <footer class="app-footer"> <!--begin::To the end-->
-            <div class="float-end d-none d-sm-inline">Anything you want</div> <!--end::To the end--> <!--begin::Copyright--> <strong>
-                Copyright &copy; 2024&nbsp;
-                <a href="https://adminlte.io" class="text-decoration-none">{{ env("APP_NAME") }}</a>.
-            </strong>
-            All rights reserved.
-            <!--end::Copyright-->
-        </footer> <!--end::Footer-->
-    </div> <!--end::App Wrapper--> <!--begin::Script--> <!--begin::Third Party Plugin(OverlayScrollbars)-->
-    <script src="https://cdn.jsdelivr.net/npm/overlayscrollbars@2.3.0/browser/overlayscrollbars.browser.es6.min.js" integrity="sha256-H2VM7BKda+v2Z4+DRy69uknwxjyDRhszjXFhsL4gD3w=" crossorigin="anonymous"></script> <!--end::Third Party Plugin(OverlayScrollbars)--><!--begin::Required Plugin(popperjs for Bootstrap 5)-->
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha256-whL0tQWoY1Ku1iskqPFvmZ+CHsvmRWx/PIoEvIeWh4I=" crossorigin="anonymous"></script> <!--end::Required Plugin(popperjs for Bootstrap 5)--><!--begin::Required Plugin(Bootstrap 5)-->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js" integrity="sha256-YMa+wAM6QkVyz999odX7lPRxkoYAan8suedu4k2Zur8=" crossorigin="anonymous"></script> <!--end::Required Plugin(Bootstrap 5)--><!--begin::Required Plugin(AdminLTE)-->
-    <script src="{{ asset("js/adminlte.js") }}"></script> <!--end::Required Plugin(AdminLTE)--><!--begin::OverlayScrollbars Configure-->
+    <!-- /.content-wrapper -->
+    <footer class="main-footer">
+        <strong>Copyright &copy; 2024 <a href="https://adminlte.io">AdminLTE.io</a>.</strong>
+        All rights reserved.
+        <div class="float-right d-none d-sm-inline-block">
+        <b>Version</b> 3.2.0
+        </div>
+    </footer>
+
+    <!-- Control Sidebar -->
+    <aside class="control-sidebar control-sidebar-dark">
+        <!-- Control sidebar content goes here -->
+    </aside>
+    <!-- /.control-sidebar -->
+    </div>
+    <!-- ./wrapper -->
+
+    <!-- jQuery -->
+    <script src="{{ asset("plugins/jquery/jquery.min.js") }}"></script>
+    <!-- jQuery UI 1.11.4 -->
+    <script src="{{ asset("plugins/jquery-ui/jquery-ui.min.js") }}"></script>
+    <!-- Resolve conflict in jQuery UI tooltip with Bootstrap tooltip -->
     <script>
-        const SELECTOR_SIDEBAR_WRAPPER = ".sidebar-wrapper";
-        const Default = {
-            scrollbarTheme: "os-theme-light",
-            scrollbarAutoHide: "leave",
-            scrollbarClickScroll: true,
-        };
-        document.addEventListener("DOMContentLoaded", function() {
-            const sidebarWrapper = document.querySelector(SELECTOR_SIDEBAR_WRAPPER);
-            if (
-                sidebarWrapper &&
-                typeof OverlayScrollbarsGlobal?.OverlayScrollbars !== "undefined"
-            ) {
-                OverlayScrollbarsGlobal.OverlayScrollbars(sidebarWrapper, {
-                    scrollbars: {
-                        theme: Default.scrollbarTheme,
-                        autoHide: Default.scrollbarAutoHide,
-                        clickScroll: Default.scrollbarClickScroll,
-                    },
-                });
-            }
-        });
-    </script> <!--end::OverlayScrollbars Configure-->
+    $.widget.bridge('uibutton', $.ui.button)
+    </script>
+    <!-- Bootstrap 4 -->
+    <script src="{{ asset("plugins/bootstrap/js/bootstrap.bundle.min.js") }}"></script>
+    <!-- ChartJS -->
+    <script src="{{ asset("plugins/chart.js/Chart.min.js") }}"></script>
+    <!-- Sparkline -->
+    <script src="{{ asset("plugins/sparklines/sparkline.js") }}"></script>
+    <!-- JQVMap -->
+    <script src="{{ asset("plugins/jqvmap/jquery.vmap.min.js") }}"></script>
+    <script src="{{ asset("plugins/jqvmap/maps/jquery.vmap.usa.js") }}"></script>
+    <!-- jQuery Knob Chart -->
+    <script src="{{ asset("plugins/jquery-knob/jquery.knob.min.js") }}"></script>
+    <!-- daterangepicker -->
+    <script src="{{ asset("plugins/moment/moment.min.js") }}"></script>
+    <script src="{{ asset("plugins/daterangepicker/daterangepicker.js") }}"></script>
+    <!-- Tempusdominus Bootstrap 4 -->
+    <script src="{{ asset("plugins/tempusdominus-bootstrap-4/js/tempusdominus-bootstrap-4.min.js") }}"></script>
+    <!-- Summernote -->
+    <script src="{{ asset("plugins/summernote/summernote-bs4.min.js") }}"></script>
+    <!-- overlayScrollbars -->
+    <script src="{{ asset("plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js") }}"></script>
+    <!-- AdminLTE App -->
+    <script src="{{ asset("dist/js/adminlte.js?v=3.2.0") }}"></script>
+    <!-- AdminLTE for demo purposes -->
+    <script src="{{ asset("dist/js/demo.js") }}"></script>
+    <!-- AdminLTE dashboard demo (This is only for demo purposes) -->
+    <script src="{{ asset("dist/js/pages/dashboard.js") }}"></script>
 
     @yield('body-js')
-</body> <!--end::Body-->
+</body>
 </html>
